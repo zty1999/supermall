ed24ef532b26c036896c4f21af4b99bce661b50e
zty1999 <1685470954@qq.com>
2019-09-09 10:49:09 +0800

    vue

--- src/App.vue
+++ src/App.vue
@@ -14,8 +14,17 @@ export default {
   name: 'app',
   components: {
     MainTabBar
+  },
+  mounted() {
+    /**
+     * 解决 css 引入图片在 github pages 无法获取的问题
+     */
+    const { NODE_ENV } = process.env;
+    document.documentElement.className = NODE_ENV;
   }
+
 }
+
 </script>
 
 <style>
