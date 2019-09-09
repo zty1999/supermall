5028e52775926ba998c352af25cbc3625bab48a7
zty1999 <1685470954@qq.com>
2019-09-09 10:44:14 +0800

    ;;;

--- src/router/index.js
+++ src/router/index.js
@@ -39,7 +39,7 @@ const router = new VueRouter({
         component: Detail
       },
     ],
-    mode: 'history'
+    // mode: 'history'
   });
 
 
--- vue.config.js
+++ vue.config.js
@@ -11,7 +11,5 @@ module.exports = {
             }
         }
     },
-    build: {
-        assetsPublicPath: './',
-    }
+    publicPath: "./",
 }
